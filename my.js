1e4a3f92344c30826c760039249351836354ad1e
´davidasamy´ <davidasamy@gmail.com>
2022-06-19 16:30:04 -0700

    camera tweaks

--- my.js
+++ my.js
@@ -1,9 +1,21 @@
-import { GLTFLoader } from "./GLTFLoader.js";
-import { FontLoader } from "./FontLoader.js";
-import { TextGeometry } from "./TextGeometry.js";
-import { OrbitControls } from "./OrbitControls.js";
-import { TWEEN } from "./tween.module.min.js"
-import { Sprite } from "./three.module.js";
+import {
+    GLTFLoader
+} from "./GLTFLoader.js";
+import {
+    FontLoader
+} from "./FontLoader.js";
+import {
+    TextGeometry
+} from "./TextGeometry.js";
+import {
+    OrbitControls
+} from "./OrbitControls.js";
+import {
+    TWEEN
+} from "./tween.module.min.js"
+import {
+    Sprite
+} from "./three.module.js";
 //import { ImageLoader } from "./ImageLoader.js";
 var scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -15,8 +27,12 @@ const camera = new THREE.PerspectiveCamera(
 const move_segment = window.innerWidth / 10 + 200
 var current_slide = 1;
 let intween = false;
-var forward = new TWEEN.Tween(camera.position).to({ x: `+${move_segment}` }, 1300).easing(TWEEN.Easing.Back.InOut);
-var backward = new TWEEN.Tween(camera.position).to({ x: `-${move_segment}` }, 1300).easing(TWEEN.Easing.Back.InOut);
+var forward = new TWEEN.Tween(camera.position).to({
+    x: `+${move_segment}`
+}, 1500).easing(TWEEN.Easing.Back.InOut);
+var backward = new TWEEN.Tween(camera.position).to({
+    x: `-${move_segment}`
+}, 1500).easing(TWEEN.Easing.Back.InOut);
 forward.onComplete(function() {
     intween = false
 })
@@ -27,14 +43,20 @@ backward.onComplete(function() {
 let body = document.getElementsByTagName("body");
 let pageX = 0.5;
 let pageY = 0.5;
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer({
+    antialias: true
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.antialias = true;
 renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
 
+
 var map = new THREE.TextureLoader().load("github.png");
-var material = new THREE.SpriteMaterial({ map: map, color: 0xffffff });
+var material = new THREE.SpriteMaterial({
+    map: map,
+    color: 0xffffff
+});
 const github = new THREE.Sprite(material);
 github.scale.set(35, 35, 1)
 github.position.x = move_segment;
@@ -42,9 +64,12 @@ github.position.z = -20
 scene.add(github);
 
 var map = new THREE.TextureLoader().load("twitter.png");
-var material = new THREE.SpriteMaterial({ map: map, color: 0xffffff });
+var material = new THREE.SpriteMaterial({
+    map: map,
+    color: 0xffffff
+});
 const twitter = new THREE.Sprite(material);
-twitter.scale.set(35, 35, 1)
+twitter.scale.set(40, 40, 1)
 twitter.position.x = 2 * (move_segment);
 twitter.position.z = -20
 twitter.position.y = 0
@@ -55,7 +80,7 @@ camera.position.set(0, 0, 40);
 
 var size;
 let loader = new FontLoader();
-loader.load('myfont2.json', function (font) {
+loader.load('myfont2.json', function(font) {
     const txt1 = new TextGeometry('WELCOME TO MY SITE', {
         font: font,
         size: window.innerWidth / 833.3333,
@@ -69,15 +94,19 @@ loader.load('myfont2.json', function (font) {
     });
     txt1.center();
     const materials = [
-        new THREE.MeshStandardMaterial({ color: 0xffffff }), // front
-        new THREE.MeshStandardMaterial({ color: 0x777777 }) // side
+        new THREE.MeshStandardMaterial({
+            color: 0xffffff
+        }), // front
+        new THREE.MeshStandardMaterial({
+            color: 0x777777
+        }) // side
     ];
     var textMesh2 = new THREE.Mesh(txt1, materials);
     textMesh2.name = "myText1";
     scene.add(textMesh2)
     textMesh2.position.y -= window.innerWidth / 250
 });
-loader.load('myfont.json', function (font) {
+loader.load('myfont.json', function(font) {
     const txt1 = new TextGeometry('davidsamy.com', {
         font: font,
         size: window.innerWidth / 200,
@@ -91,8 +120,12 @@ loader.load('myfont.json', function (font) {
     });
     txt1.center();
     const materials = [
-        new THREE.MeshStandardMaterial({ color: 0xffffff }), // front
-        new THREE.MeshStandardMaterial({ color: 0x777777 }) // side
+        new THREE.MeshStandardMaterial({
+            color: 0xffffff
+        }), // front
+        new THREE.MeshStandardMaterial({
+            color: 0x777777
+        }) // side
     ];
     var textMesh1 = new THREE.Mesh(txt1, materials);
     textMesh1.name = "myText";
@@ -101,7 +134,7 @@ loader.load('myfont.json', function (font) {
 });
 var uptween;
 var downtween;
-loader.load('myfont2.json', function (font) {
+loader.load('myfont2.json', function(font) {
     const txt1 = new TextGeometry('GitHub', {
         font: font,
         size: window.innerWidth / 150,
@@ -115,24 +148,30 @@ loader.load('myfont2.json', function (font) {
     });
     txt1.center();
     const materials = [
-        new THREE.MeshStandardMaterial({ color: 0xffffff }), // front
-        new THREE.MeshStandardMaterial({ color: 0x000000 }) // side
+        new THREE.MeshStandardMaterial({
+            color: 0xffffff
+        }), // front
+        new THREE.MeshStandardMaterial({
+            color: 0x000000
+        }) // side
     ];
     var textMesh3 = new THREE.Mesh(txt1, materials);
     textMesh3.name = "myText3";
     textMesh3.position.x = move_segment;
     textMesh3.position.y = -5
-    uptween = new TWEEN.Tween(textMesh3.position).to({ y: `+${10}` }, 2000).easing(TWEEN.Easing.Exponential.InOut
-    );
-    downtween = new TWEEN.Tween(textMesh3.position).to({ y: `-${10}` }, 2000).easing(TWEEN.Easing.Exponential.InOut
-    );
+    uptween = new TWEEN.Tween(textMesh3.position).to({
+        y: `+${10}`
+    }, 2000).easing(TWEEN.Easing.Exponential.InOut);
+    downtween = new TWEEN.Tween(textMesh3.position).to({
+        y: `-${10}`
+    }, 2000).easing(TWEEN.Easing.Exponential.InOut);
     uptween.chain(downtween);
     downtween.chain(uptween);
     scene.add(textMesh3)
 });
 var uptween2;
 var downtween2;
-loader.load('myfont2.json', function (font) {
+loader.load('myfont2.json', function(font) {
     const txt1 = new TextGeometry('Twitter', {
         font: font,
         size: window.innerWidth / 150,
@@ -146,17 +185,23 @@ loader.load('myfont2.json', function (font) {
     });
     txt1.center();
     const materials = [
-        new THREE.MeshStandardMaterial({ color: 0xffffff }), // front
-        new THREE.MeshStandardMaterial({ color: 0x1373ad }) // side
+        new THREE.MeshStandardMaterial({
+            color: 0xffffff
+        }), // front
+        new THREE.MeshStandardMaterial({
+            color: 0x1373ad
+        }) // side
     ];
     var textMesh4 = new THREE.Mesh(txt1, materials);
     textMesh4.name = "myText4";
     textMesh4.position.x = 2 * (move_segment);
     textMesh4.position.y = -5
-    uptween2 = new TWEEN.Tween(textMesh4.position).to({ y: `+${10}` }, 2000).easing(TWEEN.Easing.Exponential.InOut
-    );
-    downtween2 = new TWEEN.Tween(textMesh4.position).to({ y: `-${10}` }, 2000).easing(TWEEN.Easing.Exponential.InOut
-    );
+    uptween2 = new TWEEN.Tween(textMesh4.position).to({
+        y: `+${10}`
+    }, 2000).easing(TWEEN.Easing.Exponential.InOut);
+    downtween2 = new TWEEN.Tween(textMesh4.position).to({
+        y: `-${10}`
+    }, 2000).easing(TWEEN.Easing.Exponential.InOut);
     uptween2.chain(downtween2);
     downtween2.chain(uptween2);
     scene.add(textMesh4)
@@ -169,20 +214,19 @@ document.body.addEventListener('mousemove', (event) => {
     pageY = event.pageY / window.innerHeight;
 });
 
+
 function next() {
     if (current_slide == 1 && intween == false) {
         forward.start();
         uptween.start();
         intween = true;
-        current_slide ++;
-    }
-    else if (current_slide == 2 && intween == false) {
+        current_slide++;
+    } else if (current_slide == 2 && intween == false) {
         forward.start();
         intween = true;
-        current_slide ++;
+        current_slide++;
         uptween2.start()
-    }
-    else {
+    } else {
         console.log('Error No More To Display')
     }
 }
@@ -190,16 +234,14 @@ function next() {
 function previous() {
     if (current_slide == 1) {
         console.log('Error No More To Display')
-    }
-    else if (current_slide == 2 && intween == false) {
+    } else if (current_slide == 2 && intween == false) {
         backward.start();
         intween = true;
-        current_slide --;
-    }
-    else if (intween == false) {
+        current_slide--;
+    } else if (intween == false) {
         backward.start();
         intween = true;
-        current_slide --;
+        current_slide--;
     }
 }
 document.getElementById("rightarr").addEventListener('click', next);
@@ -211,23 +253,26 @@ var light = new THREE.AmbientLight(0xffffff);
 
 scene.add(light);
 
-var tween1 = new TWEEN.Tween(camera.position).to({ x: `+${window.innerWidth / 10}` }, 1000).easing(TWEEN.Easing.Back.InOut);
+var tween1 = new TWEEN.Tween(camera.position).to({
+    x: `+${window.innerWidth / 10}`
+}, 1000).easing(TWEEN.Easing.Back.InOut);
 
 var raycaster = new THREE.Raycaster();
 var mouse = new THREE.Vector2();
-function clickOnSprite(event){
 
-    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1; 
-    raycaster.setFromCamera( mouse, camera );   
+function clickOnSprite(event) {
 
-    let intersects = raycaster.intersectObject( github, true );
-    if (intersects.length == 1 && current_slide == 2){
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    raycaster.setFromCamera(mouse, camera);
+
+    let intersects = raycaster.intersectObject(github, true);
+    if (intersects.length == 1 && current_slide == 2) {
         console.log("clicked github");
         window.open("https://github.com/davidasamy", "_blank");
     }
-    intersects = raycaster.intersectObject( twitter, true );
-    if (intersects.length == 1 && current_slide == 3){
+    intersects = raycaster.intersectObject(twitter, true);
+    if (intersects.length == 1 && current_slide == 3) {
         console.log("clicked twitter");
         window.open("https://twitter.com/davidsamy_", "_blank");
     }
